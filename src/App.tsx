cf3878c72cafc323170d9dc83ac337f23e6c930b
phucgo240699 <phucgo240699@gmail.com>
2025-01-18 15:49:29 +0700

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { BrowserRouter } from 'react-router';
 import './App.css';
 import MarketingApp from './components/MarketingApp/index.jsx';
+import { Header } from './components/Header';
 
 const App = () => {
   return (
-    <div>
-      Container
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <Header>
+        <MarketingApp />
+      </Header>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+interface HeaderProps {
+  children: React.ReactNode;
+}
+
+export const Header: React.FC<HeaderProps> = ({ children }) => {
+  return (
+    <>
+      <div className="fixed w-full px-6 py-3 bg-white flex justify-between items-center border-b-2 border-b-neutral-300 shadow-md">
+        <p>App</p>
+        <button className="border-[1px] px-6 py-2 rounded-lg text-neutral-700 border-neutral-700 hover:text-blue-700 hover:border-blue-700 ">
+          Login
+        </button>
+      </div>
+      <br />
+      {children}
+    </>
+  );
+};
