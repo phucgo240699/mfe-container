7d7aa2fcb05e4cee68714f6283e77c2d7f738322
phucgo240699 <phucgo240699@gmail.com>
2025-01-21 10:58:38 +0700

    automatically navigate to dashboard on root path

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import './App.css';
 import { Header } from './components/Header';
 import DashboardApp from './components/DashboardApp/index.jsx';
 import MarketingApp from './components/MarketingApp/index.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header>
         <Routes>
-          <Route path="/" element={<DashboardApp />} />
+          <Route path="/" element={<Navigate to={'/dashboard'} />} />
+          <Route path="/dashboard/*" element={<DashboardApp />} />
           <Route path="/marketing/*" element={<MarketingApp />} />
         </Routes>
       </Header>
