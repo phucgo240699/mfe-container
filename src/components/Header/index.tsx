523581e0b6118146e3a80dcc327711b7ae0e89db
phucgo240699 <phucgo240699@gmail.com>
2025-01-22 10:14:44 +0700

    header navigation update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Link, NavLink } from 'react-router';
 
 interface HeaderProps {
   children?: React.ReactNode;
@@ -9,8 +8,8 @@ export const Header: React.FC<HeaderProps> = ({ children = <></> }) => {
   return (
     <>
       <div className="fixed w-full px-6 py-3 bg-white flex justify-between items-center border-b-2 border-b-neutral-300 shadow-md">
-        <NavLink to={'dashboard'}>Dashboard</NavLink>
-        <NavLink to={'marketing'}>Marketing</NavLink>
+        <a href={'/dashboard'}>Dashboard</a>
+        <a href={'/marketing'}>Marketing</a>
         <button className="border-[1px] px-6 py-2 rounded-lg text-neutral-700 border-neutral-700 hover:text-blue-700 hover:border-blue-700 ">
           Login
         </button>
