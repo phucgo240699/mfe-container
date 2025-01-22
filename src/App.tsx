3e5538d64b9d2d0bdffbbb4bf89f3b3e623c63ac
phucgo240699 <phucgo240699@gmail.com>
2025-01-22 22:11:12 +0700

    import using dynamic paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
-import { Header } from './components/Header';
-import DashboardApp from './apps/dashboard/index.jsx';
-import MarketingApp from './apps/marketing/index.jsx';
+import { Header } from '@/components/Header';
+import DashboardApp from '@/apps/dashboard/index.jsx';
+import MarketingApp from '@/apps/marketing/index.jsx';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
 
 const App = () => {
